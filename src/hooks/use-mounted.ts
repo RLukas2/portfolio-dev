42e3640d01acdc2ae6bc5aff2ac51c61a086e297
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-11-25 22:58:14 +0700

    fix: Ignore prettier used on package or lock files.

--- src/hooks/use-mounted.ts
+++ src/hooks/use-mounted.ts
@@ -1,6 +1,13 @@
 /* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useState } from 'react';
 
+/**
+ * Checks if the component is mounted.
+ * This requires eslint rule react-hooks/set-state-in-effect to be disabled.
+ * Because by default, setting state in useEffect is discouraged to avoid performance issues.
+ *
+ * @returns {boolean} - True if the component is mounted, false otherwise.
+ */
 const useMounted = () => {
   const [mounted, setMounted] = useState<boolean>(false);
 
