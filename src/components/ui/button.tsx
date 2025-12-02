4a9edd3e8c4bc257d6db2ac22e5b9063c287f75a
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-12-02 19:02:43 +0700

    chore: undo change to button size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -83,10 +83,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(
-          'min-h-[44px] min-w-[44px]',
-          buttonVariants({ variant, size, className }),
-        )}
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
