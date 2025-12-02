db3c70f3891d3a50e1f87f196b0dc2da82b22b5c
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-12-02 18:49:14 +0700

    feat: add social button to GetInTouch component

--- src/features/home/components/get-in-touch.tsx
+++ src/features/home/components/get-in-touch.tsx
@@ -1,8 +1,10 @@
 import { ChevronRightIcon, RocketIcon } from 'lucide-react';
 import Link from 'next/link';
 
+import { Email, GitHub, LinkedIn } from '@/components/common/icons';
 import { Button } from '@/components/ui/button';
 import { SITE } from '@/constants/site';
+import { cn } from '@/lib/utils';
 
 const GetInTouch = () => {
   return (
@@ -17,15 +19,50 @@ const GetInTouch = () => {
       </div>
       <p className="text-muted-foreground">
         I'm available for freelance projects and would love to explore potential
-        collaborations. Feel free to email me, and let's discuss how we can work
-        together!
+        collaborations. Feel free to reach out, and let's discuss how we can
+        work together!
       </p>
-      <Button asChild variant="shadow" className="group">
-        <Link href={`mailto:${SITE.author.email}?subject=Hi Tuan!`}>
-          Get in touch
-          <ChevronRightIcon className="transition-transform duration-200 group-hover:translate-x-1" />
-        </Link>
-      </Button>
+      <div className="flex flex-wrap gap-3">
+        <Button asChild variant="shadow" className="group gap-x-2">
+          <Link href={`mailto:${SITE.author.email}?subject=Hi Tuan!`}>
+            <Email className="size-4" />
+            Email Me
+            <ChevronRightIcon className="transition-transform duration-200 group-hover:translate-x-1" />
+          </Link>
+        </Button>
+
+        <Button
+          asChild
+          variant="outline"
+          size="icon"
+          className={cn('hover:text-[#0A66C2]')}
+        >
+          <Link
+            href={SITE.author.linkedIn}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="LinkedIn"
+          >
+            <LinkedIn className="size-4" />
+          </Link>
+        </Button>
+
+        <Button
+          asChild
+          variant="outline"
+          size="icon"
+          className={cn('hover:text-foreground')}
+        >
+          <Link
+            href={SITE.author.github.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="GitHub"
+          >
+            <GitHub className="size-4" />
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
