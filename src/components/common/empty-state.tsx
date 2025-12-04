57bddceee5e599c6d2dd332101080398048343c9
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-12-04 20:30:39 +0700

    style(empty-state): center align empty state message text

--- src/components/common/empty-state.tsx
+++ src/components/common/empty-state.tsx
@@ -12,7 +12,7 @@ const EmptyState = ({ message }: { message: string }) => {
   return (
     <div className="my-4 flex flex-col items-center justify-center space-y-1 py-3">
       <MoodSad className="size-12" />
-      <p>{message}</p>
+      <p className="text-center">{message}</p>
     </div>
   );
 };
