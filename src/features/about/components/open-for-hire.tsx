ce6c6bf62b147bd8eb3537d24a87b15f3a896acb
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2025-12-05 12:06:15 +0700

    style(about): remove the color on open for hire

--- src/features/about/components/open-for-hire.tsx
+++ src/features/about/components/open-for-hire.tsx
@@ -6,7 +6,7 @@ const OpenForHire = ({ isOpenForHire }: { isOpenForHire: boolean }) => {
       <Button
         size="sm"
         variant="outline"
-        className="pointer-events-none gap-3 border-none px-3 shadow-none"
+        className="pointer-events-none gap-3 border-none bg-transparent px-3 shadow-none"
       >
         <div className="relative flex size-2">
           <span className="absolute -top-1 -left-1 inline-flex size-4 animate-ping rounded-full bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)] opacity-75 transition-colors will-change-transform" />
